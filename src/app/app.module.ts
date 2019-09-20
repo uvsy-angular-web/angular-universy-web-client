d871c0b7af7dd5a2b1ed72559c18db3489e02058
Cami <corrales.acamila@gmail.com>
2019-09-20 19:19:40 -0300

    Agrego Componentes del Core: Footer y Home, modifico Top-nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { TopNavBarComponent } from './components/core/top-nav-bar/top-nav-bar.component';
+import { ContactComponent } from './components/core/contact/contact.component';
+import { FooterComponent } from './components/core/footer/footer.component';
+import { HomeComponent } from './components/core/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
