6e3134b1007c3cf721258dc9e993415e247d2d67
Cami <corrales.acamila@gmail.com>
2019-10-05 13:21:54 -0300

    arreglo ruta de plan

--- src/app/modules/plan/plan-routing.module.ts
+++ src/app/modules/plan/plan-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanComponent } from './pages/plan/plan.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PlanComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/plan/plan.module.ts
+++ src/app/modules/plan/plan.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlanRoutingModule } from './plan-routing.module';
-
+import {PlanComponent} from './pages/plan/plan.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PlanComponent
+  ],
   imports: [
     CommonModule,
     PlanRoutingModule
