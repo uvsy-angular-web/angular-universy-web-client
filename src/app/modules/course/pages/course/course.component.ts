89ac0c69002ea0c5a74494aa168efd842bdf4532
Maxiiiiiiiiiiiiiiiii <maxipomar@gmail.com>
2019-11-04 21:44:11 -0300

    U-106, Added function to save Button

--- src/app/modules/course/pages/course/course.component.ts
+++ src/app/modules/course/pages/course/course.component.ts
@@ -3,6 +3,8 @@ import {Course} from '../../../../models/course.model';
 import {CourseService} from '../../../../core/services/course.service';
 import {CourseModalService} from '../../modals/course-modal.service';
 import {Period} from '../../../../models/period.model';
+import {NavigationService} from '../../../../core/services/system/navigation.service';
+import {NotificationService} from '../../../../shared/modals/notification.service';
 
 @Component({
   selector: 'app-course',
@@ -13,7 +15,10 @@ export class CourseComponent implements OnInit {
 
   public course: Course;
 
-  constructor(private courseModalService: CourseModalService) {
+  constructor(private courseModalService: CourseModalService,
+              private courseService: CourseService,
+              private navigationService: NavigationService,
+              private notificationService: NotificationService) {
   }
 
   ngOnInit() {
@@ -30,8 +35,16 @@ export class CourseComponent implements OnInit {
     );
   }
 
-  saveCourse() {
-    console.table(this.course);
+  public saveCourse() {
+    this.courseService.updateCourse(this.course).subscribe(
+      () => {
+        this.notificationService.inform('Se guardo con éxito', 'Se actualizo la comisión con éxito');
+        this.navigationService.navigateToSubjectPage();
+      }, (error) => {
+        this.notificationService.showError('Ocurrió un problema tratando de publicar el plan');
+        console.error(error);
+      }
+    );
   }
 
   private addPeriod(period: Period) {
