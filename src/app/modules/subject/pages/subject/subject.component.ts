0351f219705ccb8d47ad06f8a35e2077c867a68e
Camila <corrales.acamila@gmail.com>
2019-12-03 22:37:38 -0300

    largo maximo de name comision 10 caracteres

--- src/app/modules/subject/pages/subject/subject.component.ts
+++ src/app/modules/subject/pages/subject/subject.component.ts
@@ -33,7 +33,7 @@ export class SubjectComponent implements OnInit {
       'Agregar Comisión',
       ButtonText.Add,
       '',
-      15).subscribe(
+      10).subscribe(
       (courseName) => {
         this.addCourse(courseName);
       }
