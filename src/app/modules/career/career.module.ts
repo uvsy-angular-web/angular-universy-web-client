fce73ea1fb7e6abb79b47b067593c8ad1ebb8046
Camila <corrales.acamila@gmail.com>
2019-12-04 23:11:03 -0300

    Agrego modal para career Name

--- src/app/modules/career/career.module.ts
+++ src/app/modules/career/career.module.ts
@@ -4,11 +4,15 @@ import {CommonModule} from '@angular/common';
 import {CareerRoutingModule} from './career-routing.module';
 import {SharedModule} from '../../shared/shared.module';
 import {CareerComponent} from './pages/career/career.component';
+//import { ModalsComponent } from './../../modals/modals.component';
+import { CareerModalComponent } from './modals/career-modal/career-modal.component';
 
 
 @NgModule({
   declarations: [
-    CareerComponent
+    CareerComponent,
+    //ModalsComponent,
+    CareerModalComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/career/modals/career-modal/career-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-career-modal',
+  templateUrl: './career-modal.component.html',
+  styleUrls: ['./career-modal.component.css']
+})
+export class CareerModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
