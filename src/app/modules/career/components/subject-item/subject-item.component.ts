58b1b0927b9972da76639fe184d65326db4e573e
Maxiiiiiiiiiiiiiiiii <maxipomar@gmail.com>
2020-02-16 21:48:04 -0300

    U-127, Adds progress bar to the subject rate

--- src/app/modules/career/components/subject-item/subject-item.component.ts
+++ src/app/modules/career/components/subject-item/subject-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 import { Subject } from 'src/app/models/subject.model';
+const PERCENTAGE_CONSTANT = 20;
 
 @Component({
   selector: 'app-subject-item',
@@ -9,14 +10,20 @@ import { Subject } from 'src/app/models/subject.model';
 export class SubjectItemComponent implements OnInit {
   @Input() active = false;
   @Input() subject: Subject;
+  @Input() subjectRate = Math.floor(Math.random() * 5) + 0 ;
   @Output() subjectSelected: EventEmitter<any> = new EventEmitter();
-
+  progress = 0;
   constructor() { }
 
   ngOnInit() {
+    this.calculateProgress();
   }
 
   selectSubject() {
     this.subjectSelected.emit();
+  }
+
+  private calculateProgress() {
+    this.progress = this.subjectRate * PERCENTAGE_CONSTANT;
   }
 }
