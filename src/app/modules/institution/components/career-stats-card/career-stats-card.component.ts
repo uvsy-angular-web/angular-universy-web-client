e2056bd5260d0d7f0093c5fb71d8c86503f443a1
maxipom <maxipomar@gmail.com>
2020-10-03 20:13:36 -0300

    [STATS]: Update institution report model

--- src/app/modules/institution/components/career-stats-card/career-stats-card.component.ts
+++ src/app/modules/institution/components/career-stats-card/career-stats-card.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Career } from 'src/app/models/career.model';
-import { SubjectService } from 'src/app/core/services/subject.service';
-import { Program } from 'src/app/models/program.model';
-import { ProgramService } from 'src/app/core/services/program.service';
-import { CareerService } from 'src/app/core/services/career.service';
-import { NavigationService } from 'src/app/core/services/system/navigation.service';
-import { Route } from 'src/app/core/services/system/routes/routes.enum';
+import { CareerStat } from 'src/app/models/institution-report.model';
 
 @Component({
   selector: 'app-career-stats-card',
@@ -13,26 +7,18 @@ import { Route } from 'src/app/core/services/system/routes/routes.enum';
   styleUrls: ['./career-stats-card.component.css']
 })
 export class CareerStatsCardComponent implements OnInit {
-  @Input() career: Career;
+  @Input() career: CareerStat;
   noProgramTxt = 'no posee programas';
 
-  programs: Program[] = [];
-
-  constructor(
-    private programService: ProgramService,
-    private navigationService: NavigationService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getPrograms();
   }
 
   navigateToCareerStats() {
-    CareerService.setCurrentCareer(this.career);
-    this.navigationService.navigateToRoute(Route.CAREER_STATS);
+    //T ODO: Change this after updating the career stat
+    // CareerService.setCurrentCareer(this.career);
+    // this.navigationService.navigateToRoute(Route.CAREER_STATS);
   }
 
-  private getPrograms() {
-    this.programService.getProgramsByCareer(this.career)
-      .subscribe((programs: Program[]) => this.programs = programs);
-  }
 }
