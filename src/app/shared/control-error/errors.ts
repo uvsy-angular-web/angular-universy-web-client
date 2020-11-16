d474c507b412a82d6f96d0b812f37dab73b385c5
maxipom <maxipomar@gmail.com>
2020-11-15 23:05:18 -0300

    [BUG_FIX]: Issue Fixed #79

--- src/app/shared/control-error/errors.ts
+++ src/app/shared/control-error/errors.ts
@@ -1,8 +1,8 @@
 import {InjectionToken} from '@angular/core';
 
 export const REG_EXP_ONLY_UPPERCASE_LETTERS = '^[A-Z]*$';
-export const REG_EXP_ONLY_LETTERS = '^[a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0_]+( [a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0_]+)*$';
-export const REG_EXP_ONLY_LETTERS_AND_NUMBERS = '^[a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0-9_]+( [a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0-9_]+)*$'
+export const REG_EXP_ONLY_LETTERS = '^[a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0_/*.-]+( [a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0_/*.-]+)*$';
+export const REG_EXP_ONLY_LETTERS_AND_NUMBERS = '^[a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0-9_/*.-]+( [a-zA-ZzÑñÁáÉéÍíÓóÚúÜü0-9_/*.-]+)*$'
 
 export const defaultErrors = {
   required: () => `El campo es requerido.`,
@@ -10,7 +10,7 @@ export const defaultErrors = {
     if (requiredPattern === REG_EXP_ONLY_UPPERCASE_LETTERS) {
       return 'Ingresar solo letras en mayúscula.';
     } else if (requiredPattern === REG_EXP_ONLY_LETTERS) {
-      return 'Ingresar solo letras';
+      return 'Ingresar solo caracteres válidos';
     }
     else if (requiredPattern === REG_EXP_ONLY_LETTERS_AND_NUMBERS) {
       return 'Ingresar solo Letras y/o Números. No se acepta campo vacío.';
